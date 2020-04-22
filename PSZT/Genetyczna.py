ff1fbab4f74161a48ec7dc3b0b1ce5461ece9e9d
Michał <44056903+michalaws2k18@users.noreply.github.com>
2020-04-23 00:39:16 +0200

    Update Genetyczna.py

--- PSZT/Genetyczna.py
+++ PSZT/Genetyczna.py
@@ -5,17 +5,19 @@ import copy
 niu = 200  # number of parents
 
 initialPopParameter = 50 #krotnosc wstepnego zwiekszenia poplacji poczatkowej
-N = 4
+
+N = 5
 length = N*N
-crossRate=0.7
-mutateRate=0.7
+crossRate=0.5
+mutateRate=0.5
+Proper_sum=N*(1+N*N)/2
 
 def referenceFunction():
     return N*(1+N*N)/2
 
 
 def error(vector):
-    return abs(np.sum(vector) - N*(1+N*N)/2)  #funkcja obliczająca wielkość błędu
+    return abs(np.sum(vector) - Proper_sum)  #funkcja obliczająca wielkość błędu
     # if sum(vector) == referenceFunction():
     #     return 0
     # return 1
@@ -59,19 +61,25 @@ class Population:
         self.children = []*niu
         for i in range(niu*initialPopParameter):
             self.parents.append(Subject())
-        self.calculateAllErrors()
+        # self.calculate_parents_Errors()
 
-    def updateAllMatrix(self):
+    def update_parents_Matrix(self):
         for x in self.parents:
             x.updateMatrix()
+
+    def update_children_Matrix(self):
         for x in self.children:
             x.updateMatrix()
 
-    def calculateAllErrors(self):
+    def calculate_parents_Errors(self):
         for x in self.parents:
             x.calculateError()
+    
+    def calculate_children_Errors(self):
+        for x in self.children:
+            x.calculateError()
 
-    def createChildren(self, rodzice, cuts=2):  # Krzyżowanie
+    def createChildren(self, rodzice, cuts=2, number_of_mutations=1):  # Krzyżowanie
         children = []
         for i in range(int(niu/2)):
             male = copy.deepcopy(rodzice[2*i])
@@ -80,7 +88,8 @@ class Population:
             children.append(boy)
             children.append(girl)
         self.children = children
-        self.mutation()
+        for i in range(number_of_mutations):
+            self.mutation()
 
     def mycross(self, male, female, cuts = 2):
         itsBoy = copy.deepcopy(male)
@@ -94,11 +103,11 @@ class Population:
             #do_i_change = random.randint(0, 2)
             do_i_change = random.choice([0,1], size= 1,p=[1-crossRate, crossRate])
             if (do_i_change == 1):
-                bufor = itsBoy.array[chosenCutPlaces[i]:chosenCutPlaces[i+1]]
-                for j in range(chosenCutPlaces[i], chosenCutPlaces[i+1]):
+                bufor = copy.deepcopy(itsBoy.array[chosenCutPlaces[i]:chosenCutPlaces[i+1]+1])
+                for j in range(chosenCutPlaces[i], chosenCutPlaces[i+1]+1):
                     self.swap(itsBoy.array, j, self.look_for(itsBoy.array, itsGirl.array[j]))
                 iter = 0
-                for k in range(chosenCutPlaces[i], chosenCutPlaces[i+1]):
+                for k in range(chosenCutPlaces[i], chosenCutPlaces[i+1]+1):
                     self.swap(itsGirl.array, k, self.look_for(itsGirl.array, bufor[iter]))
                     iter += 1
             else:
@@ -106,8 +115,8 @@ class Population:
         return [itsBoy, itsGirl]
 
     def swap(self, sequence, pos1, pos2):
-        bufor = sequence[pos1]
-        sequence[pos1] = sequence[pos2]
+        bufor = copy.deepcopy(sequence[pos1])
+        sequence[pos1] = copy.deepcopy(sequence[pos2])
         sequence[pos2] = bufor
 
     def look_for(self, sequence, value):
@@ -116,10 +125,21 @@ class Population:
                 return i
 
     def take_the_best(self, number):
-        self.calculateAllErrors()
+        # self.calculateAllErrors()
         self.parents.sort(key = lambda x: x.error, reverse=False)
         return copy.deepcopy(self.parents[0:number])
-
+    
+    def take_the_best_new(self, number):
+        suma = 0
+        for item in self.parents:
+            suma += item.error
+        prop=[]
+        for item in self.parents:
+            prop.append(item.error/suma)
+        parents = copy.deepcopy(self.parents)
+        wybrancy = random.choice(parents , size=number, replace=False, p=prop) 
+        return wybrancy
+        
     def mutation(self):
         for item in self.children:
             #if(random.randint(0, 1) == 0):
